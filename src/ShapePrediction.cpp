bd9ca0fbd2ea6a00687ee523d01dad52bb36bffa
tpc2005 <tpc2005@gmail.com>
2015-05-11 16:39:01 -0700

    add line 42

--- src/ShapePrediction.cpp
+++ src/ShapePrediction.cpp
@@ -39,6 +39,7 @@ std::string getResult(std::string fastaFilePath, std::string shapeType){
     
         //load the query table from file
         std::string querytable_filename = "./inst/QueryTable.dat"; //<- I don't know how to fetch the file path here!
+        //std::string querytable_filename = sourceCpp(system.file("QueryTable.dat", package="rdnashape")); // <- Can I do something like this to get the file path?
         
         process_querytable_file(querytable_filename, pentamers_map, debug);
     
